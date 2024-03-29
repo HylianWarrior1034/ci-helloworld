60a8e086abd0a7c7dd8e2b23c462aee7680287d1
HylianWarrior1034 <dm10142@gmail.com>
2024-03-29 14:22:44 -0400

    test

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -45,4 +45,14 @@ public class ArrayUtilsTest {
   public void testOddOrPositiveBothPositivesAndNegatives() {
     assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
   }
+
+  @Test
+  public void testCountOfZero() {
+    assertEquals(0, ArrayUtils.countOf(new int[]{}, 1));
+  }
+
+  @Test
+  public void testCountOfNonZero() {
+    assertEquals(1, ArrayUtils.countOf(new int[]{1}, 1));
+  }
 }
